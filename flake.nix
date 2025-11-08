68f7cff38688c51520f878348e02e0b83815fd00
Javi Merino <merino.jav@gmail.com>
2025-11-08 21:29:26 +0000

    Initial skeleton for the project

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "getsubs - download subtitles of videos";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python3;
+        pythonPackages = python.pkgs;
+      in
+      {
+        packages = {
+          default = pythonPackages.buildPythonApplication {
+            pname = "getsubs";
+            version = "0.1.0";
+            format = "pyproject";
+
+            src = ./.;
+
+            nativeBuildInputs = with pythonPackages; [
+              setuptools
+              wheel
+            ];
+
+            propagatedBuildInputs = with pythonPackages; [
+              # Add Python dependencies here
+            ];
+
+            meta = with pkgs.lib; {
+              description = "getsubs - download subtitles of videos";
+              license = licenses.mit;
+              maintainers = [ ];
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python
+            pythonPackages.pip
+            pythonPackages.setuptools
+            pythonPackages.wheel
+            # Add development dependencies here
+          ];
+
+          shellHook = ''
+            echo "getsubs development environment"
+          '';
+        };
+      }
+    );
+}
